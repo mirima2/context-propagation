258a8d0e227ecc601889256d852eb75b275cd6ea
mirima <mirima2@gmail.com>
2017-11-06 18:06:45 +0100

    change for importing from harvested oai

--- isFromzip.py
+++ isFromzip.py
@@ -11,6 +11,8 @@ from lxml import etree
 from create_is.utils import *
 
 finished = False
+split = False
+is_json = True
 
 def es_add_bulk(file = '/data/miriam/context-propagation/datacite_native.gz'):
     es = Elasticsearch(hosts=["192.168.100.108", "192.168.100.109", "192.168.100.106", "192.168.100.107"])
@@ -23,7 +25,10 @@ def es_add_bulk(file = '/data/miriam/context-propagation/datacite_native.gz'):
             pass
                       
 def parseline(line):
-    body = json.loads(line)['body']
+    if is_json:
+        body = json.loads(line)['body']
+    else:
+        body = line
     input_str =body.encode('utf-8', 'ignore')
     data = StringIO(input_str)
     root = etree.parse(data)
@@ -72,32 +77,62 @@ def get_datacite(file):
     global finished
 
     read = 0
+    indexed = 0
+    st = ''
     with gzip.open(file) as f:
         for line in f:
+            if split:
+                if '<metadata' not in line:
+                    st += line.strip()
+                    continue
+                if st == '':
+                    st = line.strip()
+                    continue
+
+                entry = st
+                st = line.strip()
+            else:
+                entry = line
 
             read += 1
             if read%1000==0:
                 print "parsed %i"%read
-           
-            try:
-                root = parseline(line)
-            except:
-                continue
+            dic = analyze_entry(entry)
 
-            ide = getidentifier(root)
-            if ide == "":          
+            if dic =={}:
                 continue
-            res = selectdocuments(root)
-
-            if res == {}:
-                continue
-            res['pid_type'] = 'doi'
-            res['provenance']=['datacite']
-            res['resource_identifier']=resource_identifier(ide,"doi")
-            res['found']=1
-            yield {'_index':'pids','_type':'known','_id':ide,'_source':res}
+           # print dic['_id']
+            print dic['_id'] , '-->', indexed
+            indexed +=1
+            yield dic
+        if split:
+            dic = analyze_entry(st)
+            if dic!= {}:
+             #   print dic['_id']
+                yield dic
 
     finished = True
+
+def analyze_entry(line):
+    try:
+        root = parseline(line)
+    except Exception as e:
+       # print e
+        return {}
+
+    ide = getidentifier(root)
+    if ide == "":
+        return {}
+    res = selectdocuments(root)
+
+    if res == {}:
+        return {}
+
+    res['pid_type'] = 'doi'
+    res['provenance'] = ['datacite']
+    res['resource_identifier'] = resource_identifier(ide, "doi")
+    res['found'] = 1
+    return {'_index': 'pids', '_type': 'known', '_id': ide, '_source': res}
 
 es_add_bulk(sys.argv[1])
 # def prova():
